2159cb04bf751b84153b6cc8a20d95ed560c4263
yosefsobol <yysobol@gmail.com>
2020-03-22 13:57:23 -0500

    Adding Routing

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
+import { BrowserRouter, Switch, Route,
+  //  Link
+   } from "react-router-dom";
+import {Login,Registration,Logout,CreateOrJoinSimulation,JoinTheSimulation,JoiningError,SimulationLibrary,SimulationLobby,PassiveParticipator,ActiveParticipator,ManagerStartSimulation,ManagerSimulationInAction} from './components'
 
 class App extends Component {
   url = "/api";
@@ -27,7 +31,26 @@ class App extends Component {
         <p>Hello Nathan Krasney</p>
         <button onClick={this.clickHandler}>Access express server !!!!!</button>
         <p>Got : {this.state.data}</p>
-      </div>
+      
+
+<BrowserRouter>
+<Switch>
+    <Route exact path='/Login' component={Login} />
+    <Route exact path='/Registration' component={Registration} />
+    <Route exact path='/Logout' component={Logout} />
+    <Route exact path='/CreateOrJoinSimulation' component={CreateOrJoinSimulation} />
+    <Route exact path='/JoinTheSimulation' component={JoinTheSimulation} />
+    <Route exact path='/JoiningError' component={JoiningError} />
+    <Route exact path='/SimulationLibrary' component={SimulationLibrary} />
+    <Route exact path='/SimulationLobby' component={SimulationLobby} />
+    <Route exact path='/PassiveParticipator' component={PassiveParticipator} />
+    <Route exact path='/ActiveParticipator' component={ActiveParticipator} />
+    <Route exact path='/ManagerStartSimulation' component={ManagerStartSimulation} />
+    <Route exact path='/ManagerSimulationInAction' component={ManagerSimulationInAction} />
+</Switch>
+</BrowserRouter>
+
+</div>
     );
   }
 }
