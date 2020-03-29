1b4af281f1c5c53d5215838e2535f1b452e8a460
agam2004 <54416773+agam2004@users.noreply.github.com>
2020-03-29 16:14:21 +0300

    adding main manu screen

---
+++ server/client/src/Main manu.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <img class="float-left" style={{marginTop: '-22%',cursor:'pointer',height:'5%',width:'15%'}} src="https://3er1viui9wo30pkxh1v2nh4w-wpengine.netdna-ssl.com/wp-content/uploads/2016/05/Microsoft-Logo-2012.jpg" alt="Microsoft"></img>
+  <div className="buttons">
+  <a href="#" class="create btn btn-primary btn-lg">הצטרפות</a>
+  <br/>
+  <a href="#" class="join btn btn-primary btn-lg">הקמה</a>
+  </div>
+  </div> 
+  );
+}
+
+export default App;
