dc1ecb11c5b636347bb976394f3ceedb7abe108d
dror krief <dror1krief@gmail.com>
2020-04-20 21:27:26 +0300

    new component CreateOrJoinSimulation

--- server/client/src/components/CreateOrJoinSimulation.js
+++ server/client/src/components/CreateOrJoinSimulation.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import  { Redirect } from 'react-router-dom'
+
 import "./CreateOrJoinSimulation.css";
 export class CreateOrJoinSimulation extends Component {
+  state={goTo:""}
     newSimulation = () =>{
         console.log("newSimulation is clicked");
-        
+        this.setState({ goTo: "SimulationLibrary" });
     }
     joinSimulation = () =>{
         console.log("joinSimulation is clicked");
+        this.setState({ goTo: "JoinTheSimulation" });
+
         
     }
   render() {
     return (
       <div className="CreateOrJoinSimulation">
+      {this.state.goTo?<Redirect to={`/${this.state.goTo}`}  />:""}
         <h1>כניסה לסימולציה</h1> <p> מנהל? צור סימולציה חדשה</p>{" "}
         <p>משתתף? הצטרף לסימולציה קיימת</p>{" "}
         <button onClick={this.newSimulation} className="btn-style">יצירת סימולציה חדשה</button>
