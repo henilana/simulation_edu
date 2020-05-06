a544f236a5b5f76f92d2c757670e0e008d430ef8
agam2004 <54416773+agam2004@users.noreply.github.com>
2020-05-06 19:50:51 +0300

    Update Button.jsx

--- server/client/src/Button.jsx
+++ server/client/src/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
+
 function Button(props) {
     return(
         <div className="button-container">
               <Link to={props.link}>
-                  <button class="rounded-0 p-3 btn btn-primary btn-lg">
+                  <button class="w-25 rounded-0 p-3 btn btn-primary btn-lg">
                         {props.ButtonName}
                   </button> 
               </Link>
