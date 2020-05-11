a28077c42ad6de302a2af5be048999734abb3faf
Aviad Gigi <Aviad1.gigi@gmail.com>
2020-05-11 11:16:42 +0300

    remove unused imports

--- server/client/src/pages/LoginSignUp.jsx
+++ server/client/src/pages/LoginSignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MyButton from '../components/MyButton';
 import Header from '../components/Header';
 import SubHeader from '../components/SubHeader'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 
 function LoginSignUp() {
   return (
@@ -12,8 +12,8 @@ function LoginSignUp() {
       <br />
       <h1>Edusim</h1>
       <Container fluid="sm" className='mt-5 d-flex justify-content-center flex-column'>
-        <MyButton classNames='mb-2 px-5 btn btn-block btn-lg' link='/Registration' ButtonName='הרשמה'/>
-        <MyButton classNames='px-5 btn btn-block btn-lg' link='/Login' ButtonName='התחברות'/>
+        <MyButton classNames='mb-2 px-5 btn btn-block btn-lg' link='/Registration' ButtonName='הרשמה' />
+        <MyButton classNames='px-5 btn btn-block btn-lg' link='/Login' ButtonName='התחברות' />
       </Container>
     </Container>
   );
