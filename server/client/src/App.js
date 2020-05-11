3a702b53feefcf6cf63c26cdc03e196206ba3c3e
Aviad Gigi <Aviad1.gigi@gmail.com>
2020-05-11 11:15:07 +0300

    align

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -12,13 +12,19 @@ import LoginSignUp from './pages/LoginSignUp'
 
 class App extends Component {
   url = "/api";
-  state = {data:''};
-  simulationsList = [{ID:1,Name:'aaaaa',
-    Desc:' Duis nisi exercitation et commodo reprehenderit minim irure pariatur exercitation anim laboris do ut ea. Est sit est voluptate sunt fugiat ut incididunt et minim anim aliquip veniam laboris.Ex laborum enim sit pariatur velit esse et ea in do duis consequat excepteur. Duis adipisicing anim duis laboris est do dolore veniam ad duis consectetur dolor laborum. Aute magna Lorem adipisicing culpa fugiat ullamco quis proident proident ullamco id ea non cupidatat. Sunt deserunt aliqua quis ex proident nulla excepteur tempor sit. Enim voluptate id sit consequat nostrud officia.  ',Time:'1:30',Participants:10},
-    {ID:2,Name:'bbbbbb',
-    Desc:' Duis nisi exercitation et commodo reprehenderit minim irure pariatur exercitation anim laboris do ut ea. Est sit est voluptate sunt fugiat ut incididunt et minim anim aliquip veniam laboris.Ex laborum enim sit pariatur velit esse et ea in do duis consequat excepteur. Duis adipisicing anim duis laboris est do dolore veniam ad duis consectetur dolor laborum. Aute magna Lorem adipisicing culpa fugiat ullamco quis proident proident ullamco id ea non cupidatat. Sunt deserunt aliqua quis ex proident nulla excepteur tempor sit. Enim voluptate id sit consequat nostrud officia.  ',Time:'2:00',Participants:4},
-    {ID:3,Name:'cccccc',
-    Desc:' Duis nisi exercitation et commodo reprehenderit minim irure pariatur exercitation anim laboris do ut ea.  Est sit est voluptate sunt fugiat ut incididunt et minim anim aliquip veniam laboris.Ex laborum enim sit pariatur velit esse et ea in do duis consequat excepteur. Duis adipisicing anim duis laboris est do dolore veniam ad duis consectetur dolor laborum. Aute magna Lorem adipisicing culpa fugiat ullamco quis proident proident ullamco id ea non cupidatat. Sunt deserunt aliqua quis ex proident nulla excepteur tempor sit. Enim voluptate id sit consequat nostrud officia. ',Time:'3:30',Participants:3}] ;
+  state = { data: '' };
+  simulationsList = [{
+    ID: 1, Name: 'aaaaa',
+    Desc: ' Duis nisi exercitation et commodo reprehenderit minim irure pariatur exercitation anim laboris do ut ea. Est sit est voluptate sunt fugiat ut incididunt et minim anim aliquip veniam laboris.Ex laborum enim sit pariatur velit esse et ea in do duis consequat excepteur. Duis adipisicing anim duis laboris est do dolore veniam ad duis consectetur dolor laborum. Aute magna Lorem adipisicing culpa fugiat ullamco quis proident proident ullamco id ea non cupidatat. Sunt deserunt aliqua quis ex proident nulla excepteur tempor sit. Enim voluptate id sit consequat nostrud officia.  ', Time: '1:30', Participants: 10
+  },
+  {
+    ID: 2, Name: 'bbbbbb',
+    Desc: ' Duis nisi exercitation et commodo reprehenderit minim irure pariatur exercitation anim laboris do ut ea. Est sit est voluptate sunt fugiat ut incididunt et minim anim aliquip veniam laboris.Ex laborum enim sit pariatur velit esse et ea in do duis consequat excepteur. Duis adipisicing anim duis laboris est do dolore veniam ad duis consectetur dolor laborum. Aute magna Lorem adipisicing culpa fugiat ullamco quis proident proident ullamco id ea non cupidatat. Sunt deserunt aliqua quis ex proident nulla excepteur tempor sit. Enim voluptate id sit consequat nostrud officia.  ', Time: '2:00', Participants: 4
+  },
+  {
+    ID: 3, Name: 'cccccc',
+    Desc: ' Duis nisi exercitation et commodo reprehenderit minim irure pariatur exercitation anim laboris do ut ea.  Est sit est voluptate sunt fugiat ut incididunt et minim anim aliquip veniam laboris.Ex laborum enim sit pariatur velit esse et ea in do duis consequat excepteur. Duis adipisicing anim duis laboris est do dolore veniam ad duis consectetur dolor laborum. Aute magna Lorem adipisicing culpa fugiat ullamco quis proident proident ullamco id ea non cupidatat. Sunt deserunt aliqua quis ex proident nulla excepteur tempor sit. Enim voluptate id sit consequat nostrud officia. ', Time: '3:30', Participants: 3
+  }];
 
   clickHandler = () => {
     console.log("clicked");
@@ -38,33 +44,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      
+
         <p><b>EDUSIM</b> Get Started ! ! !</p>
         <button onClick={this.clickHandler}>Access express server !!!!!</button>
-        <p>Got : {this.state.data}</p> 
+        <p>Got : {this.state.data}</p>
 
         <BrowserRouter>
-        <Switch>
-        <Route exact path='/' component={LoginSignUp} />
-          <Route exact path='/Login' component={Login} />
-          <Route exact path='/Registration' component={Registration} />
-          <Route exact path='/Logout' component={Logout} />
-          <Route exact path='/CreateOrJoinSimulation' component={CreateOrJoinSimulation} />
-          <Route exact path='/JoinTheSimulation' component={JoinTheSimulation} />
-          <Route exact path='/JoiningError' component={JoiningError} />
-          <Route exact path='/SimulationLibrary' component = {() => <SimulationLibrary simulationsList = {this.simulationsList} /> }/>         
-          <Route path='/Simulation/:id' component={(props) => <Simulation selectedIndex = {props.match.params.id} 
-                simulation = {this.simulationsList[props.match.params.id]} divClassName ={'SimulationLibrary'}
-                button1Link ={'/Simulation/'} button2Link ={'/SimulationLibrary'} SimulationClassName={'SimulationDetail'} descriptionClassName={'text-wrap'}
-                button1Name ={'בחירה'} button2Name = {'חזרה'}/>} /> 
-          <Route exact path='/SimulationLobby' component={SimulationLobby} />
-          <Route exact path='/PassiveParticipator' component={PassiveParticipator} />
-          <Route exact path='/ActiveParticipator' component={ActiveParticipator} />
-          <Route exact path='/ManagerStartSimulation' component={ManagerStartSimulation} />
-          <Route exact path='/ManagerSimulationInAction' component={ManagerSimulationInAction} />          
-          {/* <Route  component={NotFound}/> */}
-        </Switch>        
-      </BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={LoginSignUp} />
+            <Route exact path='/Login' component={Login} />
+            <Route exact path='/Registration' component={Registration} />
+            <Route exact path='/Logout' component={Logout} />
+            <Route exact path='/CreateOrJoinSimulation' component={CreateOrJoinSimulation} />
+            <Route exact path='/JoinTheSimulation' component={JoinTheSimulation} />
+            <Route exact path='/JoiningError' component={JoiningError} />
+            <Route exact path='/SimulationLibrary' component={() => <SimulationLibrary simulationsList={this.simulationsList} />} />
+            <Route path='/Simulation/:id' component={(props) => <Simulation selectedIndex={props.match.params.id}
+              simulation={this.simulationsList[props.match.params.id]} divClassName={'SimulationLibrary'}
+              button1Link={'/Simulation/'} button2Link={'/SimulationLibrary'} SimulationClassName={'SimulationDetail'} descriptionClassName={'text-wrap'}
+              button1Name={'בחירה'} button2Name={'חזרה'} />} />
+            <Route exact path='/SimulationLobby' component={SimulationLobby} />
+            <Route exact path='/PassiveParticipator' component={PassiveParticipator} />
+            <Route exact path='/ActiveParticipator' component={ActiveParticipator} />
+            <Route exact path='/ManagerStartSimulation' component={ManagerStartSimulation} />
+            <Route exact path='/ManagerSimulationInAction' component={ManagerSimulationInAction} />
+            {/* <Route  component={NotFound}/> */}
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
